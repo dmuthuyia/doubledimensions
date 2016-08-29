5c8aacfc315da5d8dc956dfbec84d813c1823ca8
dmutuyia <dmuthuyia@gmail.com>
2016-08-29 11:06:38 +0300

    First commit

---
+++ app/models/contact_form.rb
@@ -0,0 +1,18 @@
+class ContactForm < ActiveRecord::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :file,      :attachment => true
+
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "send us a message",
+      :to => "dennis@infohtechict.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
